a04646de9039be35ff2033cd39de2c6d163bb2e9
Oliver Andrews <oltandrews@gmail.com>
2015-05-05 20:21:45 -0700

    Initial Commit

---
+++ Math Game/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Math Game
+//
+//  Created by Oliver Andrews on 2015-05-05.
+//  Copyright (c) 2015 Oliver Andrews. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
